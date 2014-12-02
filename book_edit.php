45e664add1b049ba6249160798b6cf2adc360050
mystery28 <vladimir.axt@gmail.com>
2014-12-02 07:23:55 +0200

    Edit (add form edit book)
    Changes:
    * add new form book_edit.php
    * update database
    * fixed some bugs

---
+++ book_edit.php
@@ -0,0 +1,177 @@
+<HTML>
+  <HEAD>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html"; charset="Win1251">
+	<TITLE>book edit</TITLE>
+
+    <link rel="stylesheet" href="style_i.css" type="text/css">
+	
+	<?
+	  $bookId = ($_POST['bookId']);
+	?>
+	
+	<script type="text/javascript">
+	
+	function loadItemsTable() {
+	  var bookId = '<? echo ($bookId); ?>',
+		  typeFilter = ['%', 0, 0, 0, bookId]; 
+	
+	  if (bookId == -1) {
+	    document.getElementById("inputTitle").setAttribute('data-id', -1);
+		loadDB('query1','publisher','false',['%', 0, 0, 0,0]);
+		loadDB('query1','author','false',['%', 0, 0, 0,0]);
+		loadDB('query1','technology','false',['%', 0, 0, 0,0]);
+	  }
+	  
+	  loadDB('queryBook', 'Book', 'true', typeFilter);
+	}
+
+	function loadDB(queryType, queryData, querySelect, querySelectField, editType, editData) {
+	  var req = new XMLHttpRequest(),
+		  params = 'queryType=' + encodeURIComponent(queryType) +
+				   '&queryData=' + encodeURIComponent(queryData) +
+				   '&querySelect=' + encodeURIComponent(querySelect) +
+				   '&querySelectField=' + encodeURIComponent(JSON.stringify(querySelectField)) +
+				   '&editType=' + encodeURIComponent(editType) +
+				   '&editData=' + encodeURIComponent(JSON.stringify(editData));
+	  req.open('POST', 'work_base.php', true);
+	  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	  req.onreadystatechange = function() { loadData(req, queryType, queryData, querySelectField); };   // обработчик
+	  req.send(params);
+	}
+
+	function loadData(req,queryType,queryData,querySelectField) {
+	  if (req.readyState == 4) {
+	    if (req.status == 200) {
+	      var db = eval('(' + req.responseText + ')');
+		  		  
+		  switch (queryType) {
+			case 'query1':		loadSelectNames(db,queryData,querySelectField[4]);	break;
+			case 'queryBook':	loadInputData(db); 									break;
+			case 'exit':		document.location.href = "index.html";
+		  }
+	    }
+	  }
+	}
+
+	function loadInputData(db) {
+	  if (db) {
+		document.getElementById("inputTitle").setAttribute('data-id', db[0].id);
+		document.getElementById("inputTitle").value = db[0].title;
+
+		if (db[0].id) {
+		  loadDB('query1','publisher','false',['%', 0, 0, 0, db[0].publisher_id]);
+		  loadDB('query1','author','false',['%', 0, 0, 0, db[0].author_id]);
+		  loadDB('query1','technology','true',['%', 0, 0, 0, db[0].id]);
+		}
+
+		document.getElementById("inputDescription").value = db[0].description;
+		document.getElementById("inputFilePath").value = db[0].file_type;
+
+//		document.getElementById("selectPublisher").value = db[0].publisher_id;
+//		document.getElementById("selectAuthor").value = db[0].author_id;
+//		document.getElementById("selectTechnology").value = db[0].technology_id;
+
+//		document.getElementById("inputPublisher").setAttribute('data-id', db[0].publisher_id);
+//		document.getElementById("inputPublisher").value = db[0].publisher_name;
+//		document.getElementById("inputAuthor").setAttribute('data-id', db[0].author_id);
+//		document.getElementById("inputAuthor").value = db[0].author_name;
+	  }
+	}
+
+	function editBook() {
+	  var bookId = document.getElementById("inputTitle").getAttribute('data-id');
+		  bookTitle = document.getElementById("inputTitle").value,
+		  publisherId = document.getElementById("selectPublisher").value,
+		  authorId = document.getElementById("selectAuthor").value,
+		  technologyId = technologySelectedId(),
+		  description = document.getElementById("inputDescription").value,
+		  fileType = document.getElementById('inputFilePath').value;
+
+	  if (bookTitle == '') alert('empty title');
+	  else if (publisherId == 0) alert('select publisher');
+	  else if (authorId == 0) alert('select author');
+	  else if (!technologyId) alert('select technology');
+	  else {
+		if (bookId == -1) 
+		  loadDB('exit', 'Book', '', '', 'insertBook', [0,bookTitle,publisherId,authorId,technologyId,description,fileType]);
+		else {
+		  loadDB('exit', 'Book', '', '', 'editBook', [bookId,bookTitle,publisherId,authorId,technologyId,description,fileType]);
+		}
+	  }
+	}
+	
+	function technologySelectedId() {
+	  var result = new Array();
+	  
+	  for (var i=1; i<document.getElementById("selectTechnology").options.length; i++) {
+		if (document.getElementById("selectTechnology").options[i].selected) 
+			result.push(document.getElementById("selectTechnology").options[i].value);
+	  }
+	  
+	  return result;
+	}
+						  
+	function deleteBook() {
+	  var bookId = document.getElementById("inputTitle").getAttribute('data-id');
+		
+		if (bookId != -1) {
+		  loadDB('exit', 'Book', '', '', 'deleteBook', [bookId]);
+		}
+	}
+
+	function loadSelectNames(dbData,tableName,selectID) {
+	  var idObj = 'select'+(tableName.charAt(0).toUpperCase() + tableName.substr(1)),
+		  i = 0;
+	  
+	  document.getElementById(idObj).options[i++] = new Option('select '+tableName, '0');
+	  for (var value in dbData) {
+	    document.getElementById(idObj).options[i++] = new Option(dbData[value].name, dbData[value].id);
+	  }
+	  
+	  i = 1;
+	  if (tableName == 'technology') 
+		for (var value in dbData) {
+		  if (dbData[value].book_id == selectID) 
+			document.getElementById(idObj).options[i].selected = true;
+		  
+		  i++;
+		}
+
+	  if (tableName == 'publisher' || tableName == 'author')
+	    document.getElementById(idObj).value = selectID;
+	}
+	
+	function loadValue() {
+	  document.getElementById('inputFilePath').value = document.getElementById('selectFile').value;
+	}
+	
+	</script>
+
+  </HEAD>
+  <BODY onload='loadItemsTable();'>
+  
+  <div class="bookEditForm" id="bookEdit">
+	<p align="center">book add/edit</p>
+	<div><span class="label"> title: </span><input type="text" id="inputTitle"></div>
+	<div><span class="label"> publisher: </span><select class="selectEditBookPA" id="selectPublisher"></select></div>
+	<div><span class="label"> author: </span><select class="selectEditBookPA" id="selectAuthor"></select></div>
+	<div class="label2"> technologies: </div>
+	<div><select multiple="" id="selectTechnology"></select></div>
+	<div class="label2"> description: </div>
+	<div><textarea id="inputDescription"></textarea></div>
+	
+	<div><input type="text" id="inputFilePath"><button>Upload</button></div>
+	<div class="blockSelectFile"></div>
+	<input class="customFile" title="choose file" type="file" id="selectFile" onchange="loadValue();">
+	<div><a class="newFileButton" href="#">choose...</a></div>
+
+	<br>
+	<div id="bookEditFoot">
+	  <div class="pBookEdit"><a href="#" onClick="editBook();">[ save ]</a></div>
+	  <div class="pBookEdit"><a href="#" onClick="deleteBook();">[ delete ]</a></div>
+	  <div class="pBookEdit"><a href="index.html" onClick="">[ close ]</a></div>
+	</div>	
+  </div>
+  
+  </BODY>
+</HTML>
--- work_base.php
+++ work_base.php
@@ -22,6 +22,59 @@
 	}
 	
 	// edit db
+	$query = '';
+	if ($editType == 'insertBook'	||
+		$editType == 'editBook'	||
+		$editType == 'deleteBook') {
+	  $query = "BEGIN WORK";
+	  $result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	  
+	  if ($editType == 'insertBook') {
+		$query = "select nextval('book_id_new') as id";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+		$editData[0] = pg_fetch_object($result, 0)->id;
+
+	    $query = "INSERT INTO book(id, title, publisher_id, author_id, description, file_type) 
+				  VALUES($editData[0], '$editData[1]', $editData[2], $editData[3], '$editData[5]', '$editData[6]')";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	  }
+
+	  if ($editType == 'editBook') {
+	    $query = "UPDATE book 
+				  SET title = '$editData[1]', publisher_id = $editData[2], author_id = $editData[3], description = '$editData[5]', file_type = '$editData[6]' 
+				  WHERE id = $editData[0]";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	  }
+
+	  if ($editType == 'deleteBook') {
+	    $query = "DELETE FROM book_technology WHERE book_id = $editData[0]";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+
+	    $query = "DELETE FROM book 
+				  WHERE id = $editData[0]";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	  }
+
+	  if ($editType == 'editBook') {
+	    $query = "DELETE FROM book_technology WHERE book_id = $editData[0]";
+		$result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	  }
+	  
+	  if ($editType == 'editBook' || $editType == 'insertBook') {		
+		for ($i = 0, $size = count($editData[4]); $i < $size; $i++) {
+		  $technology_id = $editData[4][$i]; 
+		  $query = "INSERT INTO book_technology (book_id, technology_id) 
+					VALUES ($editData[0], $technology_id)";
+		  $result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+		}
+	  }
+
+	  echo($editData[0]);
+	  
+	  $query = "COMMIT";
+	  $result = pg_query($connection, $query) or die("Error in query: $query." . pg_last_error($connection));
+	}
+
 	if ($editType == 'insertAdmins'	||
 		$editType == 'editAdmins'	||
 		$editType == 'deleteAdmins') {
@@ -48,10 +101,18 @@
 	}
 
 	// query db
-	if ($queryType == 'query1') $query = "SELECT * FROM $queryData";
+	$query = '';
+	if ($queryType == 'query1') { 
+	  $query = "SELECT * FROM $queryData t";
+	  if ($querySelect == 'true') {
+		if ($querySelectField[4] != 0) { 
+		  $query .= " left join book_technology b on b.technology_id = t.id and b.book_id = $querySelectField[4] ";
+		}
+	  }
+	}
 	if ($queryType == 'queryAdmins') $query = "SELECT * FROM $queryData";
 	if ($queryType == 'queryBook') {
-	  $query = "SELECT b.id, b.title, p.name as publisher_name, a.name as author_name 
+	  $query = "SELECT b.id, b.title, b.publisher_id, p.name as publisher_name, b.author_id, a.name as author_name, b.description, b.file_type 
 			    FROM book b 
 			    INNER JOIN publisher p ON p.id = b.publisher_id 
 			    INNER JOIN author a ON a.id = b.author_id ";
@@ -64,8 +125,9 @@
 																		  FROM book_technology bt 
 																		  WHERE bt.book_id=b.id 
 																		  AND bt.technology_id IN ($querySelectField[3]))"; $prefSelect = "AND "; }
+		if ($querySelectField[4] != 0)	{ $query .= $prefSelect." b.id = $querySelectField[4] ";	$prefSelect = "AND "; }
 	  }
-	  if ($querySort <> 'none') $query .=" ORDER BY $querySortField $querySort ";
+	  if ($querySort && $querySort <> 'none') $query .=" ORDER BY $querySortField $querySort ";
 	}
 
 	// execute & return result db
